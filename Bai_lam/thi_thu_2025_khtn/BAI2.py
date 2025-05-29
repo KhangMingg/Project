f7771117dfe8ac1df7b1b3cc9977d5f35d0a89f6
macbook <khangmingg4@gmail.com>
2025-05-29 12:16:12 +0700

    Hẹ hẹ hẹ

---
+++ Bai_lam/thi_thu_2025_khtn/BAI2.py
@@ -0,0 +1,4 @@
+n = int(input())
+so_bi_chia = n * "1"
+ans = int(so_bi_chia) % 998244353
+print(ans)[]
---
+++ Bai_lam/thi_thu_2025_khtn/BAI3.py
@@ -0,0 +1,7 @@
+a, b, c = map(int, input().split())
+n, k = map(int, input().split())
+
+
+def f(a, x, b):
+    f = a * x + b
+    return f
